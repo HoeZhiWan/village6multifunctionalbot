332d87896148fc5f7383079ca32ea1c823e34f28
HoeZhiWan <m-7053769@moe-dl.edu.my>
2022-03-11 01:55:15 +0800

    test

--- test.py
+++ test.py
@@ -7,6 +7,21 @@ client = commands.Bot(command_prefix=".")
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if pf.is_profane(message.content):
+        await message.delete()
+        msg = pf.censor(message.content)
+        embed = discord.Embed(title="Profanity Detected!",description=msg, color=discord.Color.red())
+        embed.set_author(name = message.author.display_name, icon_url = message.author.avatar_url)
+        await message.channel.send(embed=embed)
+    
+    if message.content.startswith("Hello"):
+        await message.channel.send("World")
         
 @client.command()
 async def ping(ctx):
