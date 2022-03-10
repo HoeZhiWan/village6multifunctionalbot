c09fd04a28a1ae982b176965e9558b297817d148
HoeZhiWan <m-7053769@moe-dl.edu.my>
2022-03-11 02:12:08 +0800

    cogs

--- bot.py
+++ bot.py
@@ -44,5 +44,26 @@ async def ping(ctx):
 async def joke(ctx):
     joke = pyjokes.get_joke(language='en', category= 'neutral')
     await ctx.reply(f"{joke}")
+    
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send('Welcome {0.mention}.'.format(member))
+
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Hello {0.name}~'.format(member))
+        else:
+            await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
+        self._last_member = member
         
 client.run(os.environ['DISCORD_TOKEN'])
