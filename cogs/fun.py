c8ffb7b5b375a64ef6e32b64ec5a583486ec6afa
HoeZhiWan <m-7053769@moe-dl.edu.my>
2022-03-12 23:04:10 +0800

    ttoe pls work

--- cogs/fun.py
+++ cogs/fun.py
@@ -97,6 +97,13 @@ class Board():
 def run_game():
   pass
 
+def ball_gen(question):
+    responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+                "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+                "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
+                "Yes.", "Yes – definitely.", "You may rely on it."]
+    return f"Question: {question}\nAnswer: {random.choice(responses)}"
+
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -107,6 +114,7 @@ class Fun(commands.Cog):
         self.db = {}
         self.db['turn'] = 0
         self.db['started'] = False
+        self.db['cp'] = "X"
 
     @commands.command()
     async def emoji(self, ctx):
